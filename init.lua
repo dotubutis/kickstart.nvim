7024a326768409ec2f45c13cb30fb1898b3428c2
dotubutis <dotubutis@gmail.com>
2024-11-30 22:15:03 -0500

    update nvim-ts-autotag config

--- init.lua
+++ init.lua
@@ -1046,20 +1046,9 @@ require('lazy').setup({
   },
   {
     'windwp/nvim-ts-autotag',
-    opts = {
-      -- Defaults
-      enable_close = true, -- Auto close tags
-      enable_rename = true, -- Auto rename pairs of tags
-      enable_close_on_slash = false, -- Auto close on trailing </
-    },
-    -- Also override individual filetype configs, these take priority.
-    -- Empty by default, useful if one of the "opts" global settings
-    -- doesn't work well in a specific filetype
-    per_filetype = {
-      ['html'] = {
-        enable_close = false,
-      },
-    },
+    config = function()
+      require('nvim-ts-autotag').setup()
+    end,
   },
   { 'tpope/vim-fugitive' },
   { 'tpope/vim-rails' },
