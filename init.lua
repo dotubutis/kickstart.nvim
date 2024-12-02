5c3731ac25824ec17fda47e141e21e108444d37c
Donatas Tubutis <dotubutis@gmail.com>
2024-12-02 12:50:44 -0500

    add a profiling script

--- init.lua
+++ init.lua
@@ -430,6 +430,24 @@ require('lazy').setup({
       vim.keymap.set('n', '<leader>sn', function()
         builtin.find_files { cwd = vim.fn.stdpath 'config' }
       end, { desc = '[S]earch [N]eovim files' })
+
+      -- Profiling --
+      -- wk.register({ p = { name = 'Perf Profiling' } }, { prefix = '<leader>u' })
+
+      vim.keymap.set('n', '<leader>ups', function()
+        vim.cmd [[
+              :profile start /tmp/nvim-profile.log
+                  :profile func *
+                      :profile file *
+                        ]]
+      end, { desc = 'Profile Start' })
+
+      vim.keymap.set('n', '<leader>upe', function()
+        vim.cmd [[
+              :profile stop
+                  :e /tmp/nvim-profile.log
+                    ]]
+      end, { desc = 'Profile End' })
     end,
   },
   {
