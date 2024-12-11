8a1cdd1ebefc9582d99a980ec5994f1e4cce532b
Donatas Tubutis <dotubutis@gmail.com>
2024-12-11 16:42:22 -0500

    add gitsigns keymaps

--- init.lua
+++ init.lua
@@ -273,6 +273,34 @@ require('lazy').setup({
         topdelete = { text = '‾' },
         changedelete = { text = '~' },
       },
+      on_attach = function(bufnr)
+        local gitsigns = require 'gitsigns'
+
+        local function map(mode, l, r, opts)
+          opts = opts or {}
+          opts.buffer = bufnr
+          vim.keymap.set(mode, l, r, opts)
+        end
+
+        -- Navigation
+        map('n', ']c', function()
+          if vim.wo.diff then
+            vim.cmd.normal { ']c', bang = true }
+          else
+            gitsigns.nav_hunk 'next'
+          end
+        end, { desc = 'Next Git Hunk' })
+
+        map('n', '[c', function()
+          if vim.wo.diff then
+            vim.cmd.normal { '[c', bang = true }
+          else
+            gitsigns.nav_hunk 'prev'
+          end
+        end, { desc = 'Previous Git Hunk' })
+
+        map('n', '<leader>tb', gitsigns.toggle_current_line_blame)
+      end,
     },
   },
 
